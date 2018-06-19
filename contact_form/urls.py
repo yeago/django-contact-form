4dcc9e8bca759d1849b9867e0f215e15e50cbf28
ivaggione <ivaggione@gmail.com>
2018-06-19 19:28:26 +0000

    Django 1.11.4

--- contact_form/urls.py
+++ contact_form/urls.py
@@ -10,19 +10,14 @@ hierarchy (for best results with the defaults, include it under
 
 """
 
-try:
-    from django.conf.urls.defaults import patterns, url
-except ImportError:
-    from django.conf.urls import patterns, url
+
+from django.conf.urls import url
 from django.views.generic import TemplateView
 from contact_form.views import contact_form
 
 
-urlpatterns = patterns('',
-                       url(r'^$',
-                           contact_form,
-                           name='contact_form'),
-                       url(r'^sent/$',
-                           TemplateView.as_view(template_name="contact_form/contact_form_sent.html"),
-                           name='contact_form_sent'),
-                       )
+urlpatterns = [
+    url(r'^$', contact_form, name='contact_form'),
+    url(r'^sent/$', TemplateView.as_view(template_name="contact_form/contact_form_sent.html"),
+        name='contact_form_sent')
+]
